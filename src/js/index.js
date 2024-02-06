adcc5bd51214e72ecee9982a9b98714ab019f66e
Otavio-Barra <otaviombarra@gmail.com>
2024-02-06 06:16:49 -0300

    ajaietando menu desktop

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 const menuHamburguer = document.querySelector("#navMenu");
-const menu = document.querySelector(".menu");
+let menu = document.querySelector(".menu");
 const menuMobile = document.querySelector(".menu__navigation-mobile");
 
 menuHamburguer.addEventListener("click", () => {
@@ -9,13 +9,26 @@ menuHamburguer.addEventListener("click", () => {
 });
 
 const submenu = document.querySelectorAll(".activeSubMenu");
+const teste = document.querySelectorAll(".fa-chevron-left");
+const ulSubMenuActived = document.querySelectorAll(".link__submenu");
 
-for (const menu of submenu) {
-  menu.addEventListener("click", (e) => {
-    e.target.parentElement.children[1].classList.add("active");
+for (let i = 0; i < submenu.length; i++) {
+  const subMenuAtual = submenu[i];
+  subMenuAtual.addEventListener("click", () => {
+    menu.classList.add("active");
+    if (!ulSubMenuActived[i].classList.contains("active")) {
+      const submenuAtivo = document.querySelector(".link__submenu.active");
+      console.log(submenuAtivo);
+      if (submenuAtivo) {
+        submenuAtivo.classList.remove("active");
+      }
+      ulSubMenuActived[i].classList.add("active");
+    }
   });
 }
 
 function closeSubmenu(e) {
-  e.target.parentElement.classList.remove("active");
+  menu.classList.remove("active");
+  const close = e.target.parentElement;
+  close.classList.remove("active");
 }
